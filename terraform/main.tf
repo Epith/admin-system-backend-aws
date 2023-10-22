7161ad9317cc57d2e46dc16c01ab5fcf48fe49f4
S0methingSimple <30189981+S0methingSimple@users.noreply.github.com>
2023-10-22 15:46:42 +0800

    feat: update terraform dynamodb
    Signed-off-by: S0methingSimple <30189981+S0methingSimple@users.noreply.github.com>

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,19 +8,44 @@ terraform {
 }
 
 provider "aws" {
-  region = "ap-southeast-1"
+    region = "ap-southeast-1"
 }
 
-resource "aws_dynamodb_table" "users" {
-  name = "users"
-  attribute_definitions = [
-    { attribute_name = "id", attribute_type = "S" },
-  ]
-  key_schema = [
-    { attribute_name = "id", key_type = "HASH" },
-  ]
-  provisioned_throughput {
-    read_capacity_units = 5
-    write_capacity_units = 5
-  }
+resource "aws_dynamodb_table" "users_table" {
+    name = "users"
+    billing_mode = "PAY_PER_REQUEST"
+    hash_key = "id"
+    attribute {
+        name = "id"
+        type = "S"
+    }
+
+    tags = {
+        Name = "dynamodb-users"
+    }
+}
+
+resource "aws_dynamodb_table" "points_table" {
+    name = "points"
+    billing_mode = "PAY_PER_REQUEST"
+    hash_key = "id"
+
+    attribute {
+        name = "id"
+        type = "S"
+    }
+    attribute {
+        name = "user_id"
+        type = "S"
+    }
+
+    global_secondary_index {
+        name = "user_id-index"
+        hash_key = "user_id"
+        projection_type = "ALL"
+    }
+
+    tags = {
+        Name = "points"
+    }
 }

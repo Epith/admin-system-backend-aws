645e74da77a6da6264b8b59c1c1ee5d7fbe11db3
Hei Yee Kai <endurrus@gmail.com>
2023-10-27 22:57:47 +0800

    fix(debug): debugging

--- terraform/main.tf
+++ terraform/main.tf
@@ -32,11 +32,12 @@ resource "aws_dynamodb_table" "points_table" {
     range_key = "points_id"
 
     attribute {
-        name = "points_id"
+        name = "user_id"
         type = "S"
     }
+
     attribute {
-        name = "user_id"
+        name = "points_id"
         type = "S"
     }
 

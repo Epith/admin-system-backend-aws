e8f57b023160f8de7febdab0f9b9e8f6c97b316a
S0methingSimple <30189981+S0methingSimple@users.noreply.github.com>
2023-11-02 23:40:45 +0800

    feat: update roles perms
    Signed-off-by: S0methingSimple <30189981+S0methingSimple@users.noreply.github.com>

--- terraform/seeding/roles_creation.sh
+++ terraform/seeding/roles_creation.sh
@@ -1,23 +1,24 @@
+# get post put delete patch
 aws dynamodb put-item \
   --table-name roles \
   --item '{
     "owner": {
       "M": {
         "users": {"S": "11110"},
-        "points": {"S": "11100"},
+        "points": {"S": "10100"},
         "logs": {"S": "10000"},
-        "maker": {"S": "00000"},
-        "checker": {"S": "11000"},
+        "maker": {"S": "10000"},
+        "checker": {"S": "10100"},
         "roles": {"S": "11110"}
       }
     },
     "manager": {
       "M": {
         "users": {"S": "11100"},
-        "points": {"S": "11100"},
+        "points": {"S": "10100"},
         "logs": {"S": "10000"},
-        "maker": {"S": "11110"},
-        "checker": {"S": "11000"},
+        "maker": {"S": "10000"},
+        "checker": {"S": "10100"},
         "roles": {"S": "10000"}
       }
     },
@@ -26,8 +27,8 @@ aws dynamodb put-item \
         "users": {"S": "10000"},
         "points": {"S": "10000"},
         "logs": {"S": "10000"},
-        "maker": {"S": "11110"},
-        "checker": {"S": "10000"},
+        "maker": {"S": "11000"},
+        "checker": {"S": "00000"},
         "roles": {"S": "10000"}
       }
     },
@@ -36,8 +37,8 @@ aws dynamodb put-item \
         "users": {"S": "10000"},
         "points": {"S": "10000"},
         "logs": {"S": "00000"},
-        "maker": {"S": "11110"},
-        "checker": {"S": "10000"},
+        "maker": {"S": "11000"},
+        "checker": {"S": "00000"},
         "roles": {"S": "10000"}
       }
     }

8f377aae777485baa23097d14789741559f376a3
Chia Kwee Cheng <kcchia.2021@scis.smu.edu.sg>
2023-11-05 13:34:04 +0800

    refactor: remove unnecessary logging

--- functions/delete-roles/main.go
+++ functions/delete-roles/main.go
@@ -2,37 +2,19 @@ package main
 
 import (
 	"errors"
-	"log"
 	"os"
-	"regexp"
-	"strings"
-	"time"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
-	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
-	"github.com/google/uuid"
 )
 
 type Role struct {
 	Role   string              `json:"role"`
 	Access map[string][]string `json:"access"`
-}
-
-type Log struct {
-	Log_ID          string      `json:"log_id"`
-	Severity        int         `json:"severity"`
-	User_ID         string      `json:"user_id"`
-	Action_Type     int         `json:"action_type"`
-	Resource_Type   string      `json:"resource_type"`
-	Body            interface{} `json:"body"`
-	QueryParameters interface{} `json:"query_parameters"`
-	Error           interface{} `json:"error"`
-	Timestamp       time.Time   `json:"timestamp"`
 }
 
 var (
@@ -76,10 +58,6 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 		}, nil
 	}
 
-	if logErr := sendLogs(request, 2, 4, "role", dynaClient, err); logErr != nil {
-		log.Println("Logging err :", logErr)
-	}
-
 	return events.APIGatewayProxyResponse{
 		Body:       "Role ID missing",
 		StatusCode: 404,
@@ -99,12 +77,9 @@ func DeleteRole(id string, req events.APIGatewayProxyRequest, tableName string, 
 
 	result, err := dynaClient.GetItem(checkRole)
 	if err != nil {
-		if logErr := sendLogs(req, 3, 4, "role", dynaClient, err); logErr != nil {
-			log.Println("Logging err :", logErr)
-		}
 		return errors.New(ErrorFailedToFetchRecordID)
 	}
-	
+
 	if result.Item == nil {
 		return errors.New(ErrorRoleDoesNotExist)
 	}
@@ -120,14 +95,7 @@ func DeleteRole(id string, req events.APIGatewayProxyRequest, tableName string, 
 	}
 	_, err = dynaClient.DeleteItem(input)
 	if err != nil {
-		if logErr := sendLogs(req, 3, 4, "role", dynaClient, err); logErr != nil {
-			log.Println("Logging err :", logErr)
-		}
 		return errors.New(ErrorCouldNotDeleteItem)
-	}
-
-	if logErr := sendLogs(req, 1, 4, "role", dynaClient, err); logErr != nil {
-		log.Println("Logging err :", logErr)
 	}
 
 	return nil
@@ -135,51 +103,4 @@ func DeleteRole(id string, req events.APIGatewayProxyRequest, tableName string, 
 
 func main() {
 	lambda.Start(handler)
-}
-
-func sendLogs(req events.APIGatewayProxyRequest, severity int, action int, resource string, dynaClient dynamodbiface.DynamoDBAPI, err error) error {
-	LOGS_TABLE := os.Getenv("LOGS_TABLE")
-	//create log struct
-	log := Log{}
-	log.Body = RemoveNewlineAndUnnecessaryWhitespace(req.Body)
-	log.QueryParameters = req.QueryStringParameters
-	log.Error = err
-	log.Log_ID = uuid.NewString()
-	log.Severity = severity
-	log.User_ID = req.RequestContext.Identity.User
-	log.Action_Type = action
-	log.Resource_Type = resource
-	log.Timestamp = time.Now().UTC()
-
-	av, err := dynamodbattribute.MarshalMap(log)
-
-	if err != nil {
-		return errors.New("failed to marshal log")
-	}
-
-	input := &dynamodb.PutItemInput{
-		Item:      av,
-		TableName: aws.String(LOGS_TABLE),
-	}
-	_, err = dynaClient.PutItem(input)
-	if err != nil {
-		return errors.New("Could not dynamo put")
-	}
-	return nil
-}
-
-func RemoveNewlineAndUnnecessaryWhitespace(body string) string {
-	// Remove newline characters
-	body = regexp.MustCompile(`\n|\r`).ReplaceAllString(body, "")
-
-	// Remove unnecessary whitespace
-	body = regexp.MustCompile(`\s{2,}|\t`).ReplaceAllString(body, " ")
-
-	// Remove the character `\"`
-	body = regexp.MustCompile(`\"`).ReplaceAllString(body, "")
-
-	// Trim the body
-	body = strings.TrimSpace(body)
-
-	return body
 }
--- functions/update-roles/main.go
+++ functions/update-roles/main.go
@@ -3,11 +3,7 @@ package main
 import (
 	"encoding/json"
 	"errors"
-	"log"
 	"os"
-	"regexp"
-	"strings"
-	"time"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -16,7 +12,6 @@ import (
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
-	"github.com/google/uuid"
 )
 
 type Role struct {
@@ -24,26 +19,13 @@ type Role struct {
 	Access map[string][]string `json:"access"`
 }
 
-type Log struct {
-	Log_ID          string      `json:"log_id"`
-	Severity        int         `json:"severity"`
-	User_ID         string      `json:"user_id"`
-	Action_Type     int         `json:"action_type"`
-	Resource_Type   string      `json:"resource_type"`
-	Body            interface{} `json:"body"`
-	QueryParameters interface{} `json:"query_parameters"`
-	Error           interface{} `json:"error"`
-	Timestamp       time.Time   `json:"timestamp"`
-}
-
 var (
-	ErrorFailedToUnmarshalRecord = "failed to unmarshal record"
-	ErrorInvalidRoleData         = "invalid role data"
-	ErrorInvalidRole             = "invalid role"
-	ErrorCouldNotMarshalItem     = "could not marshal item"
-	ErrorCouldNotDynamoPutItem   = "could not dynamo put item"
-	ErrorRoleDoesNotExist        = "role does not exist"
-	ErrorFailedToFetchRecordID   = "failed to fetch record by role"
+	ErrorInvalidRoleData       = "invalid role data"
+	ErrorInvalidRole           = "invalid role"
+	ErrorCouldNotMarshalItem   = "could not marshal item"
+	ErrorCouldNotDynamoPutItem = "could not dynamo put item"
+	ErrorRoleDoesNotExist      = "role does not exist"
+	ErrorFailedToFetchRecordID = "failed to fetch record by role"
 )
 
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
@@ -83,10 +65,6 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 		}, nil
 	}
 
-	if logErr := sendLogs(request, 2, 3, "role", dynaClient, err); logErr != nil {
-		log.Println("Logging err :", logErr)
-	}
-
 	return events.APIGatewayProxyResponse{
 		StatusCode: 404,
 		Body:       string("Invalid role data"),
@@ -99,18 +77,12 @@ func UpdateRole(id string, req events.APIGatewayProxyRequest, tableName string, 
 
 	//unmarshal body into role struct
 	if err := json.Unmarshal([]byte(req.Body), &role); err != nil {
-		if logErr := sendLogs(req, 2, 3, "role", dynaClient, err); logErr != nil {
-			log.Println("Logging err :", logErr)
-		}
 		return nil, errors.New(ErrorInvalidRoleData)
 	}
 	role.Role = id
 
 	if role.Role == "" {
 		err := errors.New(ErrorInvalidRole)
-		if logErr := sendLogs(req, 2, 3, "role", dynaClient, err); logErr != nil {
-			log.Println("Logging err :", logErr)
-		}
 		return nil, err
 	}
 
@@ -126,21 +98,15 @@ func UpdateRole(id string, req events.APIGatewayProxyRequest, tableName string, 
 
 	result, err := dynaClient.GetItem(checkRole)
 	if err != nil {
-		if logErr := sendLogs(req, 3, 1, "role", dynaClient, err); logErr != nil {
-			log.Println("Logging err :", logErr)
-		}
 		return nil, errors.New(ErrorFailedToFetchRecordID)
 	}
 
 	if result.Item == nil {
-		return nil, errors.New("role does not exist")
+		return nil, errors.New(ErrorRoleDoesNotExist)
 	}
 
 	av, err := dynamodbattribute.MarshalMap(role)
 	if err != nil {
-		if logErr := sendLogs(req, 3, 3, "role", dynaClient, err); logErr != nil {
-			log.Println("Logging err :", logErr)
-		}
 		return nil, errors.New(ErrorCouldNotMarshalItem)
 	}
 
@@ -151,14 +117,7 @@ func UpdateRole(id string, req events.APIGatewayProxyRequest, tableName string, 
 
 	_, err = dynaClient.PutItem(input)
 	if err != nil {
-		if logErr := sendLogs(req, 3, 3, "role", dynaClient, err); logErr != nil {
-			log.Println("Logging err :", logErr)
-		}
 		return nil, errors.New(ErrorCouldNotDynamoPutItem)
-	}
-
-	if logErr := sendLogs(req, 1, 3, "role", dynaClient, err); logErr != nil {
-		log.Println("Logging err :", logErr)
 	}
 
 	return &role, nil
@@ -166,51 +125,4 @@ func UpdateRole(id string, req events.APIGatewayProxyRequest, tableName string, 
 
 func main() {
 	lambda.Start(handler)
-}
-
-func sendLogs(req events.APIGatewayProxyRequest, severity int, action int, resource string, dynaClient dynamodbiface.DynamoDBAPI, err error) error {
-	LOGS_TABLE := os.Getenv("LOGS_TABLE")
-	//create log struct
-	log := Log{}
-	log.Body = RemoveNewlineAndUnnecessaryWhitespace(req.Body)
-	log.QueryParameters = req.QueryStringParameters
-	log.Error = err
-	log.Log_ID = uuid.NewString()
-	log.Severity = severity
-	log.User_ID = req.RequestContext.Identity.User
-	log.Action_Type = action
-	log.Resource_Type = resource
-	log.Timestamp = time.Now().UTC()
-
-	av, err := dynamodbattribute.MarshalMap(log)
-
-	if err != nil {
-		return errors.New("failed to marshal log")
-	}
-
-	input := &dynamodb.PutItemInput{
-		Item:      av,
-		TableName: aws.String(LOGS_TABLE),
-	}
-	_, err = dynaClient.PutItem(input)
-	if err != nil {
-		return errors.New("Could not dynamo put")
-	}
-	return nil
-}
-
-func RemoveNewlineAndUnnecessaryWhitespace(body string) string {
-	// Remove newline characters
-	body = regexp.MustCompile(`\n|\r`).ReplaceAllString(body, "")
-
-	// Remove unnecessary whitespace
-	body = regexp.MustCompile(`\s{2,}|\t`).ReplaceAllString(body, " ")
-
-	// Remove the character `\"`
-	body = regexp.MustCompile(`\"`).ReplaceAllString(body, "")
-
-	// Trim the body
-	body = strings.TrimSpace(body)
-
-	return body
 }

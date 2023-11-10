0a1e752fcc383de2448ad5e887fdd8a80d8c1611
Chia Kwee Cheng <kcchia.2021@scis.smu.edu.sg>
2023-11-11 02:03:22 +0800

    feat: logging for delete users

--- functions/delete-users/main.go
+++ functions/delete-users/main.go
@@ -4,8 +4,7 @@ import (
 	"errors"
 	"log"
 	"os"
-	"regexp"
-	"strings"
+	"strconv"
 	"time"
 
 	"github.com/aws/aws-lambda-go/events"
@@ -28,15 +27,12 @@ type User struct {
 }
 
 type Log struct {
-	Log_ID          string      `json:"log_id"`
-	Severity        int         `json:"severity"`
-	User_ID         string      `json:"user_id"`
-	Action_Type     int         `json:"action_type"`
-	Resource_Type   string      `json:"resource_type"`
-	Body            interface{} `json:"body"`
-	QueryParameters interface{} `json:"query_parameters"`
-	Error           interface{} `json:"error"`
-	Timestamp       time.Time   `json:"timestamp"`
+	Log_ID      string `json:"log_id"`
+	IP          string `json:"ip"`
+	Description string `json:"description"`
+	UserAgent   string `json:"user_agent"`
+	Timestamp   int64  `json:"timestamp"`
+	TTL         int64  `json:"ttl"`
 }
 
 var (
@@ -81,10 +77,6 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 		}, nil
 	}
 
-	if logErr := sendLogs(request, 2, 4, "user", dynaClient, err); logErr != nil {
-		log.Println("Logging err :", logErr)
-	}
-
 	return events.APIGatewayProxyResponse{
 		Body:       "User ID missing",
 		StatusCode: 404,
@@ -104,23 +96,17 @@ func DeleteUser(id string, role string, req events.APIGatewayProxyRequest, table
 
 	result, err := dynaClient.GetItem(checkUser)
 	if err != nil {
-		if logErr := sendLogs(req, 3, 4, "user", dynaClient, err); logErr != nil {
-			log.Println("Logging err :", logErr)
-		}
 		return errors.New(ErrorFailedToFetchRecordID)
 	}
 
 	var user User
 	if result.Item == nil {
 		return errors.New(ErrorUserDoesNotExist)
-	} else {
-		err = dynamodbattribute.UnmarshalMap(result.Item, &user)
-		if err != nil {
-			if logErr := sendLogs(req, 2, 2, "user", dynaClient, err); logErr != nil {
-				log.Println("Logging err :", logErr)
-			}
-			return errors.New(ErrorFailedToUnmarshal)
-		}
+	}
+
+	err = dynamodbattribute.UnmarshalMap(result.Item, &user)
+	if err != nil {
+		return errors.New(ErrorFailedToUnmarshal)
 	}
 
 	//attempt to delete user in cognito
@@ -145,13 +131,11 @@ func DeleteUser(id string, role string, req events.APIGatewayProxyRequest, table
 	}
 	_, err = dynaClient.DeleteItem(input)
 	if err != nil {
-		if logErr := sendLogs(req, 3, 4, "user", dynaClient, err); logErr != nil {
-			log.Println("Logging err :", logErr)
-		}
 		return errors.New(ErrorCouldNotDeleteItem)
 	}
 
-	if logErr := sendLogs(req, 1, 4, "user", dynaClient, err); logErr != nil {
+	//logging
+	if logErr := sendLogs(req, dynaClient, user.FirstName, user.LastName); logErr != nil {
 		log.Println("Logging err :", logErr)
 	}
 
@@ -162,20 +146,30 @@ func main() {
 	lambda.Start(handler)
 }
 
-func sendLogs(req events.APIGatewayProxyRequest, severity int, action int, resource string, dynaClient dynamodbiface.DynamoDBAPI, err error) error {
+func sendLogs(req events.APIGatewayProxyRequest, dynaClient dynamodbiface.DynamoDBAPI, firstName string, lastName string) error {
+	// Calculate the TTL value (one month from now)
+	TTL := os.Getenv("TTL")
+	ttlNum, err := strconv.Atoi(TTL)
+	if err != nil {
+		return errors.New("invalid ttl")
+	}
+
+	now := time.Now()
+	oneWeekFromNow := now.AddDate(0, 0, ttlNum)
+	ttlValue := oneWeekFromNow.Unix()
+
+	//requester
 	LOGS_TABLE := os.Getenv("LOGS_TABLE")
+	requester := req.QueryStringParameters["requester"]
+
 	//create log struct
 	log := Log{}
-	log.Body = RemoveNewlineAndUnnecessaryWhitespace(req.Body)
-	log.QueryParameters = req.QueryStringParameters
-	log.Error = err
 	log.Log_ID = uuid.NewString()
-	log.Severity = severity
-	log.User_ID = req.RequestContext.Identity.User
-	log.Action_Type = action
-	log.Resource_Type = resource
-	log.Timestamp = time.Now().UTC()
-
+	log.IP = req.Headers["x-forwarded-for"]
+	log.UserAgent = req.Headers["user-agent"]
+	log.TTL = ttlValue
+	log.Description = requester + " deleted user " + firstName + " " + lastName
+	log.Timestamp = time.Now().Unix()
 	av, err := dynamodbattribute.MarshalMap(log)
 
 	if err != nil {
@@ -190,21 +184,6 @@ func sendLogs(req events.APIGatewayProxyRequest, severity int, action int, resou
 	if err != nil {
 		return errors.New("Could not dynamo put")
 	}
+
 	return nil
-}
-
-func RemoveNewlineAndUnnecessaryWhitespace(body string) string {
-	// Remove newline characters
-	body = regexp.MustCompile(`\n|\r`).ReplaceAllString(body, "")
-
-	// Remove unnecessary whitespace
-	body = regexp.MustCompile(`\s{2,}|\t`).ReplaceAllString(body, " ")
-
-	// Remove the character `\"`
-	body = regexp.MustCompile(`\"`).ReplaceAllString(body, "")
-
-	// Trim the body
-	body = strings.TrimSpace(body)
-
-	return body
 }
